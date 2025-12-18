ee317f41781e082d0b764bba6797cc83ef20add8
Pavel Marek <pavel.marek@enso.org>
2025-12-18 18:25:34 +0100

    Add WindowsFetchFromJarTest

---
+++ src/test/java/org/akirathan/windows/nio/WindowsFetchFromJarTest.java
@@ -0,0 +1,73 @@
+package org.akirathan.windows.nio;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.containsString;
+
+
+public class WindowsFetchFromJarTest {
+  @Rule
+  public TemporaryFolder tmpFolder = new TemporaryFolder();
+  private Path zipFilePath;
+
+  @Before
+  public void setup() throws IOException {
+    var tmpDir = tmpFolder.newFolder();
+    var subdir = tmpDir.toPath().resolve("subidr");
+    subdir.toFile().mkdirs();
+    var fileInSubdir = subdir.resolve("file.txt");
+    Files.writeString(fileInSubdir, "This is a test file.", StandardOpenOption.CREATE_NEW);
+    zipFilePath = tmpDir.toPath().resolve("test.zip");
+    zipDirectory(tmpDir.toPath(), zipFilePath);
+    assert zipFilePath.toFile().exists();
+  }
+
+  @Test
+  public void testFetchFileFromZip_ViaURLConnection() throws Exception {
+    var zipFileURI = zipFilePath.toUri();
+    var fileInJarURI = URI.create("jar:" + zipFileURI + "!/subidr/file.txt");
+    var conn = fileInJarURI.toURL().openConnection();
+    try (var is = conn.getInputStream()) {
+      var content = new String(is.readAllBytes());
+      assertThat(content, is("This is a test file."));
+    }
+  }
+
+  private static void zipDirectory(Path dirToZip, Path zipOut) throws IOException {
+    try (var outputStream =
+             Files.newOutputStream(
+                 zipOut, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+      try (var zipOutStream = new ZipOutputStream(outputStream)) {
+        try (var inputDirStream = Files.walk(dirToZip)) {
+          inputDirStream
+              .filter(path -> !Files.isDirectory(path))
+              .forEach(
+                  path -> {
+                    var zipEntryName = dirToZip.relativize(path).toString();
+                    try {
+                      zipOutStream.putNextEntry(new ZipEntry(zipEntryName));
+                      Files.copy(path, zipOutStream);
+                      zipOutStream.closeEntry();
+                    } catch (IOException e) {
+                      throw new AssertionError(e);
+                    }
+                  });
+        }
+      }
+    }
+  }
+}
